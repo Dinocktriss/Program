c7607e99a78ae35fb86b37615b5e3917cdb0d6de
Dinocktriss <hobu4oku@gmail.com>
2024-11-23 17:12:05 +0900

    1,1
    добавлено обновление где есть все дополнительные функции библиотеки и опыт взаимодействия библиотеки стал более интересным

--- Program/Program.cs
+++ Program/Program.cs
@@ -1,5 +1,7 @@
 ﻿using System;
+using System.IO;
 using MyLibrary;
+
 namespace Program
 {
     internal class Program
@@ -20,6 +22,54 @@ namespace Program
             Console.WriteLine($"Method3: {result1}");
             Console.WriteLine($"Method4: {result2}");
             Console.WriteLine($"Method5: {result3}");
+
+            // Тестирование метода чтения файла
+            try
+            {
+                string fileContent = myClass.ReadFile("example.txt");
+                Console.WriteLine("Содержимое файла: " + fileContent);
+            }
+            catch (FileNotFoundException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine($"Произошла ошибка: {e.Message}");
+            }
+
+            // Тестирование метода сортировки массива
+            int[] array = { 5, 3, 8, 2, 1 };
+            int[] sortedArray = myClass.SortArray(array);
+            Console.WriteLine("Отсортированный массив: " + string.Join(", ", sortedArray));
+
+            // Ввод дат и вычисление разницы между ними
+            DateTime date1, date2;
+
+            while (true)
+            {
+                Console.WriteLine("Введите первую дату (в формате ГГГГ-ММ-ДД):");
+                if (DateTime.TryParse(Console.ReadLine(), out date1))
+                {
+                    break;
+                }
+                Console.WriteLine("Неправильный формат даты. Пожалуйста, введите дату в формате ГГГГ-ММ-ДД.");
+            }
+
+            while (true)
+            {
+                Console.WriteLine("Введите вторую дату (в формате ГГГГ-ММ-ДД):");
+                if (DateTime.TryParse(Console.ReadLine(), out date2))
+                {
+                    break;
+                }
+                Console.WriteLine("Неправильный формат даты. Пожалуйста, введите дату в формате ГГГГ-ММ-ДД.");
+            }
+
+            TimeSpan difference = myClass.CalculateDateDifference(date1, date2);
+            Console.WriteLine($"Разница между датами: {difference.Days} дней");
+
+            Console.WriteLine("Завершение работы программы.");
         }
     }
 }
